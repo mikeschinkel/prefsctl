851c9a8e90843f9196b5a33fc70ab71f7856b6b0
Mike Schinkel <mike@newclarity.net>
2024-11-17 21:32:12 -0500

    Add GPLv3 `LICENSE`

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
-import "C"
 import (
 	"errors"
 	"fmt"
 	"reflect"
 	"strings"
 
-	"prefsctl/macprefs"
+	"github.com/mikeschinkel/prefsctl/macprefs"
 )
+
+/*
+   Copyright (C) 2024 Mike Schinkel
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
 
 func main() {
 	domains, err := macprefs.GetPreferenceDomains()
